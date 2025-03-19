14b4cf1be9741030e8874129fae43894c8afdd77
mseragiotto <matteo.seragiotto94@hotmail.it>
2025-03-19 22:50:32 +0100

    Initial Commit

---
+++ ArtworkRecognitionDemo/ArtworkRecognitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtworkRecognitionDemoApp.swift
+//  ArtworkRecognitionDemo
+//
+//  Created by Matteo Seragiotto on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtworkRecognitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtworkRecognitionDemoTests/ArtworkRecognitionDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtworkRecognitionDemoTests.swift
+//  ArtworkRecognitionDemoTests
+//
+//  Created by Matteo Seragiotto on 19/03/25.
+//
+
+import Testing
+@testable import ArtworkRecognitionDemo
+
+struct ArtworkRecognitionDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
